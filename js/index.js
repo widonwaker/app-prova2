b7afac765ce08403306836ef78e75041f102da7f
widonwaker <hey.lol@live.it>
2016-05-25 13:59:38 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,49 @@
+function initPushwoosh() {
+	var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+	if(device.platform == "Android")
+	{
+		registerPushwooshAndroid();
+	}
+
+	if(device.platform == "iPhone" || device.platform == "iOS")
+	{
+		registerPushwooshIOS();
+	}
+
+	if (device.platform == "Win32NT") {
+	    registerPushwooshWP();
+	}
+}
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        initPushwoosh();
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
