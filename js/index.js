a819f0c37c3af84e89fb16a6b4687cdbc8cf8b02
widonwaker <hey.lol@live.it>
2016-05-26 12:59:07 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,49 +1,18 @@
-function initPushwoosh() {
-	var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
-	if(device.platform == "Android")
-	{
-		registerPushwooshAndroid();
-	}
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
 
-	if(device.platform == "iPhone" || device.platform == "iOS")
-	{
-		registerPushwooshIOS();
-	}
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
+  };
 
-	if (device.platform == "Win32NT") {
-	    registerPushwooshWP();
-	}
-}
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        initPushwoosh();
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
+  window.plugins.OneSignal.init("3018b031-ab1f-4e8c-ba10-796d54419bbb",
+                                 {googleProjectNumber: "152595972803"},
+                                 notificationOpenedCallback);
+  
+  // Show an alert box if a notification comes in when the user is in your app.
+  window.plugins.OneSignal.enableInAppAlertNotification(true);
+}, false);
